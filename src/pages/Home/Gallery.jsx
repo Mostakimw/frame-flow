7e580cf8dc1f2523bfa5a2636bcbd05c0307586b
Mostakimw <scientist.expert@gmail.com>
2023-11-05 03:15:03 +0600

    file input text color changed

--- src/pages/Home/Gallery.jsx
+++ src/pages/Home/Gallery.jsx
@@ -163,8 +163,8 @@ const Gallery = () => {
         <form className="md:flex justify-center gap-4 space-y-3 md:space-y-0 mb-12">
           {/* image upload input field  */}
           <input
-            className="relative m-0 block cursor-pointer rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] font-normal leading-[2.15] text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-gray-700 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-blue-700 dark:file:text-neutral-100 dark:focus:border-primary"
-            id="formFileLg"
+            className="relative m-0 block cursor-pointer rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] font-normal leading-[2.15] text-black transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-black file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-gray-700 focus:border-primary focus:text-black focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-black dark:file:bg-blue-700 dark:file:text-neutral-100 dark:focus:border-primary"
+            id="imageUploadInput"
             type="file"
             onChange={(event) => {
               setImages(event.target.files[0]);
